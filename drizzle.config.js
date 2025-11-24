5243909e5a1e6c1ea429e44ee0fa7d023051aa64
Shivang Sagar <shivang26sagar@gmail.com>
2025-11-24 14:53:01 +0530

    Fix: Added @vitejs/plugin-react, updated vite config, and dbConfig changes

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,12 +1,12 @@
 // drizzle.config.js
 import { defineConfig } from 'drizzle-kit';
+import 'dotenv/config';
 
 export default defineConfig({
   schema: './src/utils/schema.jsx',
   out: './drizzle',
   dialect: 'postgresql',
   dbCredentials: {
-    // This is YOUR new, clean database. This is correct.
-    url: "postgresql://neondb_owner:npg_Vitn6rYvQp5g@ep-solitary-bonus-a1xn12cz-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
+    url: meta.env.VITE_NEON_DATABASE_URL,
   },
 });
--- src/utils/dbConfig.jsx
+++ src/utils/dbConfig.jsx
@@ -1,7 +1,6 @@
 import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
-const sql = neon(
- "postgresql://neondb_owner:npg_Vitn6rYvQp5g@ep-solitary-bonus-a1xn12cz-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"   
- );
+const sql = neon(import.meta.env.VITE_NEON_DATABASE_URL);
+
 export const db = drizzle(sql, { schema });
